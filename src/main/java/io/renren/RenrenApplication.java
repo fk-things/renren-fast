eb998cdd803d3b7c7344f3783ccb25a2ced2829b
pf.developer@outlook.com <pf.developer@outlook.com>
2018-07-13 18:24:43 +0800

    activi工作引擎添加

--- src/main/java/io/renren/RenrenApplication.java
+++ src/main/java/io/renren/RenrenApplication.java
@@ -1,6 +1,9 @@
 package io.renren;
 
 import io.renren.datasources.DynamicDataSourceConfig;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -15,6 +18,17 @@ public class RenrenApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RenrenApplication.class, args);
+
+		ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
+				.setJdbcUrl("jdbc:h2:mem:activiti;DB_CLOSE_DELAY=1000")
+				.setJdbcUsername("sa")
+				.setJdbcPassword("")
+				.setJdbcDriver("org.h2.Driver")
+				.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+		ProcessEngine processEngine = cfg.buildProcessEngine();
+		String pName = processEngine.getName();
+		String ver = ProcessEngine.VERSION;
+		System.out.println("ProcessEngine [" + pName + "] Version: [" + ver + "]");
 	}
 
 	@Override
